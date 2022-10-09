f095447cdc5924467da44c8047e905fef48eb9de
eniiku <enikuauth@gmail.com>
2022-10-09 17:17:10 +0100

    style(tailwind): create custom theme for tailwind & uninstall plugin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#A4C7C6',
+        secondary: '#FACD66',
+        white: '#EFEEE0',
+        black: '#1D2123',
+        'black-alt': '#1A1E1F',
+      },
+    },
   },
-  plugins: [require('daisyui')],
+  plugins: [],
 };
